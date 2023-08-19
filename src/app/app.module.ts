43cd57dcf9db39c6cdbce2db2572a820fddd1346
Sandra <sandrarequena1999@gmail.com>
2023-08-19 02:57:57 +0200

    Proyecto Angular básico

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
-import { APP_ROUTES } from './app.routes';
-import { NavbarComponent } from './navbar/navbar.component';
-import { HomeComponent } from './home/home.component';
-import { InitProgJavaComponent } from './init-prog-java/init-prog-java.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
-    HomeComponent,
-    InitProgJavaComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    RouterModule,
-    RouterModule.forRoot(APP_ROUTES, {
-      anchorScrolling: 'enabled'
-    })
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
