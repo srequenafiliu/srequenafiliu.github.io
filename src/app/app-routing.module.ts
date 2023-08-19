c49d86f459f1ae7f028f657eae7427b342d3f7c7
Sandra <sandrarequena1999@gmail.com>
2023-08-19 04:02:07 +0200

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,16 +4,14 @@ import { HomeComponent } from './home/home.component';
 import { InitProgJavaComponent } from './init-prog-java/init-prog-java.component';
 
 const routes: Routes = [
-  /*{ path: 'inicio', component: HomeComponent, title: "Portfolio" },
+  { path: 'inicio', component: HomeComponent, title: "Portfolio" },
   { path: 'init-prog-java', component: InitProgJavaComponent, title: "Portfolio | InitProgJava" },
   { path: '', redirectTo: '/inicio', pathMatch: 'full' }, // Ruta por defecto (vacía)
-  { path: '**', redirectTo: '/inicio', pathMatch: 'full' }*/ // Ruta que no coincide con ninguna de las anteriores
+  { path: '**', redirectTo: '/inicio', pathMatch: 'full' } // Ruta que no coincide con ninguna de las anteriores
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {
-    anchorScrolling: 'enabled'
-  })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
